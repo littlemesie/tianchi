9346000436c66a3f2a67ac88856d6f534baabd36
littlemesie <littlemesie@163.com>
2019-04-03 20:38:51 +0800

    user_cf

---
+++ src/fresh_comp/readme.md
@@ -0,0 +1,1 @@
+### 天池新人实战赛之[离线赛](https://tianchi.aliyun.com/competition/entrance/231522/introduction)
---
+++ src/fresh_comp/utils.py
@@ -0,0 +1,66 @@
+#! /usr/bin/python3
+# coding=utf-8
+import os
+import random
+import pickle
+
+base_path ="/Volumes/d/fresh_comp_offline/"
+# base_path = os.path.dirname(os.path.abspath(__file__)) + "/../../data/fresh_comp/"
+
+def loadfile(filename):
+    """
+    根据文件名载入数据
+    :param filename:
+    :return:
+    """
+    with open(filename, "r") as f:
+        for i, line in enumerate(f):
+            if i == 0:
+                continue
+            yield line
+
+
+def read_train_user_data(path=base_path + "tianchi_fresh_comp_train_user.csv", train_rate=1., seed=1):
+
+    """
+    载入评分数据
+    @param path:  文件路径
+    @param train_rate:   训练集所占整个数据集的比例，默认为1，表示所有的返回数据都是训练集
+    @return: (训练集，测试集)
+    """
+    trainset = list()
+    testset = list()
+    random.seed(seed)
+    for line in loadfile(filename=path):
+        arr_list = line.split(',')
+        user = arr_list[0]
+        item = arr_list[1]
+        if random.random() < train_rate:
+            trainset.append([int(user), int(item)])
+        else:
+            testset.append([int(user), int(item)])
+    return trainset, testset
+
+def save_file(filepath, data):
+    """
+        保存数据
+        @param filepath:    保存路径
+        @param data:    要保存的数据
+    """
+    parent_path = filepath[: filepath.rfind("/")]
+
+    if not os.path.exists(parent_path):
+        os.mkdir(parent_path)
+    with open(filepath, "wb") as f:
+        pickle.dump(data, f)
+
+
+def load_file(filepath):
+    """载入二进制数据"""
+    with open(filepath, "rb") as f:
+        data = pickle.load(f)
+    return data
+
+if __name__ == '__main__':
+    trainset, testset = read_train_user_data()
+    print(trainset)
